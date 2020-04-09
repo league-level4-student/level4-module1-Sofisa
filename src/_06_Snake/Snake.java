c2fe806f2b4b523bef65e60d3486840a88338dcf
Sofia Velarde <sofiavelarde2004@gmail.com>
2020-04-08 19:54:57 -0700

    Snake
    Finished, slight debugging needed

--- src/_06_Snake/Snake.java
+++ src/_06_Snake/Snake.java
@@ -77,37 +77,52 @@ public class Snake {
 	public void setDirection(Direction d) {
 		//1. set the current direction equal to the passed in Direction only if canMove is true.
 		//   set canMove equal to false.
-		//   make sure the snake cannot completely reverse directions.
-		//currentDirection = 
+		//   make sure the snake cannot completely reverse directions. 
+		
+		
+		//????
+		
+		
+		if (canMove == true) {
+			currentDirection = d;
+			canMove = false;
+		}
 	}
 
 	public void reset(Location loc) {
 		//1. clear the snake
-		
+		snake.clear();
 		//2. set the location of the head
-		
+		head.setLocation(loc);
 		//3. add the head to the snake
-		
+		snake.add(head);
 	}
 
 	public boolean isOutOfBounds() {
 		//1. complete the method so it returns true if the head of the snake is outside of the window
 		//   and false otherwise
-		
+		if (head.getLocation().y <= 600 || head.getLocation().x <= 750) {
+			return true;
+		}else {
 		return false;
+		}
 	}
 	
 	public boolean isHeadCollidingWithBody() {
 		//1. complete the method so it returns true if the head is located
 		//   in the same location as any other body segment
-		
+		if (head.getLocation().equals(snake)) {
+			return true;
+		}
 		return false;
 	}
 
 	public boolean isLocationOnSnake(Location loc) {
 		//1. complete the method so it returns true if the passed in
 		//   location is located on the snake
-		
+		if (loc.equals(snake)) {
+			return true;
+		}
 		return false;
 	}
 
